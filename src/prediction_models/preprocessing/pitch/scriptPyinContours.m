3fe4271708e5bd1cad4a9f2d0e98213559fcd9f4
Ashis Pati <ashis.pati.89@gmail.com>
2017-11-03 21:17:46 -0400

    Added pYin pitch contours for Bb Clarinet and Flute, 2013, 2014, 2015

--- src/prediction_models/preprocessing/pitch/scriptPyinContours.m
+++ src/prediction_models/preprocessing/pitch/scriptPyinContours.m
@@ -1,11 +1,16 @@
 band = 'middle';
-inst = 'Alto Saxophone';
+inst = 'Flute';
 
 year = '2013';
 savePyinPitchContours(band, inst, year);
+copyPyinPitchContours(band, inst, year);
 
 year = '2014';
 savePyinPitchContours(band, inst, year);
+copyPyinPitchContours(band, inst, year);
 
 year = '2015';
 savePyinPitchContours(band, inst, year);
+copyPyinPitchContours(band, inst, year);
+
+
