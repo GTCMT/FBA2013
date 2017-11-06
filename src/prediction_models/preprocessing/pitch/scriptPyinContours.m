068ad38ea387f05246680a4e018169c900959050
Ashis Pati <ashis.pati.89@gmail.com>
2017-11-06 18:21:18 -0500

    Added pYin contours for symphonic band Flute 2013

--- src/prediction_models/preprocessing/pitch/scriptPyinContours.m
+++ src/prediction_models/preprocessing/pitch/scriptPyinContours.m
@@ -1,16 +1,16 @@
-band = 'middle';
+band = 'symphonic';
 inst = 'Flute';
 
 year = '2013';
 savePyinPitchContours(band, inst, year);
 copyPyinPitchContours(band, inst, year);
 
-year = '2014';
-savePyinPitchContours(band, inst, year);
-copyPyinPitchContours(band, inst, year);
+%year = '2014';
+%savePyinPitchContours(band, inst, year);
+%copyPyinPitchContours(band, inst, year);
 
-year = '2015';
-savePyinPitchContours(band, inst, year);
-copyPyinPitchContours(band, inst, year);
+%year = '2015';
+%savePyinPitchContours(band, inst, year);
+%copyPyinPitchContours(band, inst, year);
 
 
